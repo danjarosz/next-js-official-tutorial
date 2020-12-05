3a414fe7263bff270bdd444617f1c4bc6589350a
danjarosz <jarosz.daniel@protonmail.com>
2020-12-05 23:09:02 +0100

    feat: add FirstPage and Link

---
+++ pages/posts/first-post.js
@@ -0,0 +1,12 @@
+import Link from "next/link";
+
+const FirstPost = () => {
+  return (
+    <>
+      <h1>First Post</h1>
+      <Link href="/"><a>Go to the main page</a></Link>
+    </>
+  )
+}
+
+export default FirstPost;
