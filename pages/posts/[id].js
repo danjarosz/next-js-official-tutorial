da852eded06b4650b5623bb77a7cf4e462a10866
danjarosz <jarosz.daniel@protonmail.com>
2020-12-10 22:12:26 +0100

    feat([id].js): add Head and title

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -1,5 +1,6 @@
+import Head from "next/head";
 import Layout from "../../components/layout";
-import { getAllPostIds, getPostData } from '../../lib/posts'
+import { getAllPostIds, getPostData } from '../../lib/posts';
 
 export async function getStaticProps({ params }) {
   const postData = await getPostData(params.id)
@@ -21,6 +22,9 @@ export async function getStaticPaths() {
 export default function Post({ postData }) {
   return (
     <Layout>
+      <Head>
+        <title>{postData.title}</title>
+      </Head>
       {postData.title}
       <br />
       {postData.id}
